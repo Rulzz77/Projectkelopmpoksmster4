1607bf6f33f4ecf53150dab4fb9ff56efabdd449
Rulzz77 <bahrululumsidik452@gmail.com>
2025-04-14 18:50:34 +0700

    mengganti banner

--- resources/views/v_layouts/app.blade.php
+++ resources/views/v_layouts/app.blade.php
@@ -45,7 +45,7 @@
         <div id="top-header">
             <div class="container">
                 <div class="pull-left">
-                    <span>Selamat datang di toko online</span>
+                    <span>Selamat datang di penyewaan apartemen </span>
                 </div>
             </div>
         </div>
